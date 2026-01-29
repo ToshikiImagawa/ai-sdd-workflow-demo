f043ec8a4ce81157e9d1bfc8d1933c5adcd1c9cc
Toshiki Imagawa <toshiki.imagawa@dena.com>
2026-01-30 01:05:48 +0900

    [refactor] `HierarchyFlowVisual` にノードアニメーションを追加
    - `nodePulse` アニメーションを新規追加し、CSS モジュールを更新
    - 各ノードに異なるタイミングでのアニメーションを適用し、視覚効果を強化

--- src/visuals/HierarchyFlowVisual.tsx
+++ src/visuals/HierarchyFlowVisual.tsx
@@ -9,19 +9,19 @@ export function HierarchyFlowVisual() {
     <div className={styles['hierarchy-visual-wrapper']}>
       <div className={styles['hierarchy-flow-particle']} />
 
-      <div className={`${styles['hierarchy-node']} ${styles['hierarchy-primary']}`}>
+      <div className={`${styles['hierarchy-node']} ${styles['hierarchy-primary']} ${styles['node-pulse-1']}`}>
         <BalanceIcon /> CONSTITUTION (PRD)
       </div>
       <div className={styles['hierarchy-arrow']} />
-      <div className={`${styles['hierarchy-node']} ${styles['hierarchy-standard']}`}>
+      <div className={`${styles['hierarchy-node']} ${styles['hierarchy-standard']} ${styles['node-pulse-2']}`}>
         <DescriptionIcon /> Specification
       </div>
       <div className={styles['hierarchy-arrow']} />
-      <div className={`${styles['hierarchy-node']} ${styles['hierarchy-standard']}`}>
+      <div className={`${styles['hierarchy-node']} ${styles['hierarchy-standard']} ${styles['node-pulse-3']}`}>
         <DesignServicesIcon /> Design
       </div>
       <div className={styles['hierarchy-arrow']} />
-      <div className={`${styles['hierarchy-node']} ${styles['hierarchy-ephemeral']}`}>
+      <div className={`${styles['hierarchy-node']} ${styles['hierarchy-ephemeral']} ${styles['node-pulse-4']}`}>
         <PlaylistAddCheckIcon /> Tasks (Temporary)
       </div>
     </div>
